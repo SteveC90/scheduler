0cbaa2667e7863a3badccc5f6e4dd7f551df0582
Esteban <echavez@mines.edu>
2013-04-14 22:21:08 -0600

    sections form has now been updated. Checkboxes and time fields have been added. Still needs work. Instructor not in form yet.

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,4 +2,9 @@ class Section < ActiveRecord::Base
   belongs_to :instructor
   belongs_to :course
   attr_accessible :crn, :days, :section, :term, :time_end, :time_start, :year, :course_id
+  validates :crn, :section, :term, :year, presence: true
+  validates_associated :instructor, :course
+  serialize :days, Hash
+  serialize :time_start, Hash
+  serialize :time_end, Hash
 end
