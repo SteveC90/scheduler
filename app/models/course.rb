b16922d0dd6f5d009bfd6997d60b960d09a9c3ba
Esteban <echavez@mines.edu>
2013-04-14 22:52:04 -0600

    updated views. sections form is complete. consider removing checkboxes and update the show for sections.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
   belongs_to :subject
   attr_accessible :name, :number, :subject_id
+  has_many :sections
   validates :name, :number, :subject, presence: true
   validates :number, presence: true, length: { is: 3}, numericality: { only_integer: true }
   validates_associated :subject
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,6 @@
 class Instructor < ActiveRecord::Base
   attr_accessible :email, :name
+  has_many :sections
   validates :name, :email, presence: true, uniqueness: true
   validates :email, email_format: {message: 'format is incorrect'}
 end
