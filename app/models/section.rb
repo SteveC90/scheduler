0752d71465145a246c4c51667acb7b3ae3479e1b
Esteban <echavez@mines.edu>
2013-04-14 23:02:21 -0600

    removed days from section model as well as checkboxes from form

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,12 +2,11 @@ class Section < ActiveRecord::Base
 	TERMS = ["Spring", "Fall", "Summer One", "Summer Two"]
   belongs_to :instructor
   belongs_to :course
-  attr_accessible :crn, :days, :section, :term, :time_end, :time_start, :year, :course_id, :instructor_id
+  attr_accessible :crn, :section, :term, :time_end, :time_start, :year, :course_id, :instructor_id
   validates :crn, :section, :term, :year, :course_id, :instructor_id, presence: true
   validates :year, :crn, numericality: { only_integer: true }
   validates :year, length: { is: 4 }
   validates_associated :instructor, :course
-  serialize :days, Hash
   serialize :time_start, Hash
   serialize :time_end, Hash
 end
---
+++ db/migrate/20130415045851_remove_days_from_section.rb
@@ -0,0 +1,7 @@
+class RemoveDaysFromSection < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
